e3a16629b010b83676016d5570f2c7a808f5fdbb
Brad <brad@mycnl.com>
2013-12-21 16:59:08 -0700

    updated launch script to properly name files when zipping them to avoid overwriting old log files

--- bin/gekko_launch.sh
+++ bin/gekko_launch.sh
@@ -2,10 +2,12 @@
 cd ~/gekko
 
 # zip previous log files for this config
-zip -vu log/gekko_logs.zip log/gekko_log.$1.txt
+now="`date +%Y%m%d%H%M%S`"
+mv log/gekko_log.$1.txt log/gekko_log.$1.$now.txt
+zip -vu log/gekko_logs.zip log/gekko_log.$1.$now.txt
 
 # remove raw text files now that they're zipped
-rm log/gekko_log.$1.txt
+rm log/gekko_log.$1.$now.txt
 
 # finally launch gekko and log output to log file as well as stdout
 node gekko config="config/user/$1.js" 2>&1 | tee log/gekko_log.$1.txt
