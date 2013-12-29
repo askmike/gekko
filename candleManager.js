da2629cf07b17683737148aab7635d4d923604b6
Mike van Rossum <mike@askmike.org>
2013-12-29 13:42:01 +0100

    bug in time calculation

--- candleManager.js
+++ candleManager.js
@@ -18,22 +18,25 @@ var Manager = function() {
   this.model = require('./databaseManager');
   this.model.setRealCandleSize(config.EMA.interval);
 
-  if(!config.backtest.enabled) {
+  if(config.backtest.enabled) {
+    console.log('WUP WUP this.backtest();');
+  } else {
     // watch the market
     var TradeFetcher = require('./tradeFetcher');
     this.fetcher = new TradeFetcher;
+
     // we pass a fetch to the model right away
     // so it knows how new the history needs to be
     this.fetcher.once('new trades', this.model.init);
+    this.model.on('history', this.processHistory);
 
     this.model.once('history', _.bind(function(history) {
-      this.processHistory(history);
+      // the first time the model first needs to calculate
+      // the available history, after it did this it will
+      // process the trades itself
       this.fetcher.on('new trades', this.model.processTrades);
     }, this));
-
-  } else
-    console.log('WUP WUP this.backtest();');
-
+  }
 }
 
 Manager.prototype.setupAdvice = function() {
@@ -43,22 +46,20 @@ Manager.prototype.setupAdvice = function() {
 Manager.prototype.processHistory = function(history) {
   // if history is not in this region we should discard
   var minimum = util.intervalsAgo(config.EMA.candles);
-  var requiredHistory = config.EMA.candles * config.EMA.interval;
+  var requiredHistory = util.minToMs(config.EMA.candles * config.EMA.interval);
 
   if(!this.exchange.providesHistory) {
-
     if(history.empty) {
-      // we are just going to fetch because we don't have
-      // any history yet.
+      // we don't have any history yet
       log.info('No history found, starting to build one now');
       var startAt = utc().add('ms', requiredHistory);
-      var startIn = utc().from(startAt);
       log.info(
         'Expected to start giving advice',
-        startIn,
+        startAt.fromNow(),
         '(' + startAt.format('YYYY-MM-DD HH:mm:ss') + ' UTC)'
       );
     } else if(!history.complete) {
+      // we do have history but it's not complete
       log.debug('History found, but not enough to start giving advice');
       log.info(
         'I have history since',
@@ -66,13 +67,13 @@ Manager.prototype.processHistory = function(history) {
         '(' + history.start.format('YYYY-MM-DD HH:mm:ss') + ' UTC)'
       );
       var startAt = history.start.clone().add('ms', requiredHistory);
-      var startIn = utc().from(startAt);
       log.info(
         'Expected to start giving advice', 
-        startIn,
+        startAt.fromNow(),
         '(' + startAt.format('YYYY-MM-DD HH:mm:ss') + ' UTC)'
       );
     } else {
+      // we have complete history
       log.info('Full history available');
       console.log('GOT FULL HISTORY, SIZE:', history.candles.length);
     }
