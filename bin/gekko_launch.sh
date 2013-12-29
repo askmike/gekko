b65f62349f2cafbd1c9fd6a9d76379d56790e658
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2013-12-29 21:05:33 +0000

    Only archive old logfile if it exists.

--- bin/gekko_launch.sh
+++ bin/gekko_launch.sh
@@ -3,12 +3,16 @@ cd ~/gekko
 
 # zip previous log files for this config
 now="`date +%Y%m%d%H%M%S`"
-mv log/gekko_log.$1.txt log/gekko_log.$1.$now.txt
-zip -vu log/gekko_logs.zip log/gekko_log.$1.$now.txt
+current_log=log/gekko_log.$1.txt
+archive_log=log/gekko_log.$1.$now.txt
 
-# remove raw text files now that they're zipped
-rm log/gekko_log.$1.$now.txt
+if [ -f $current_log ]; then
+    mv log/gekko_log.$1.txt $archive_log
+    zip -vu log/gekko_logs.zip $archive_log
+
+    # remove raw text files now that they're zipped
+    rm $archive_log
+fi
 
 # finally launch gekko and log output to log file as well as stdout
-node gekko config="config/user/$1.js" 2>&1 | tee log/gekko_log.$1.txt
-
+node gekko config="config/user/$1.js" 2>&1 | tee $current_log
