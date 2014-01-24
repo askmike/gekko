e0cf4983c4e138e8c6ab6afffdadf060ebdae4d5
Mike van Rossum <mike@askmike.org>
2014-01-24 17:41:04 +0100

    fixed #175

--- core/portfolioManager.js
+++ core/portfolioManager.js
@@ -227,7 +227,7 @@ Manager.prototype.sell = function(amount, price) {
   var availabe = this.getBalance(this.asset);
 
   // if not suficient funds
-  if(amount > availabe) {
+  if(amount < availabe) {
     return log.info(
       'wanted to buy but insufficient',
       this.asset,
