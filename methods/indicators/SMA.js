7a32e6c2da6169aa52150fe9536fa15a026344ff
Christian Eiden <christian@eiden.ch>
2014-02-16 20:38:05 +0100

    New indicator SMA

---
+++ methods/indicators/SMA.js
@@ -0,0 +1,17 @@
+// required indicators
+
+var Indicator = function(weight) {
+  this.weight = weight;
+  this.prices = new Array();
+  this.result = 0;
+  this.age = 0;
+}
+
+Indicator.prototype.update = function(price) {
+  this.prices[this.age % this.weight] = price;
+  sum = this.prices.reduce(function(a, b) { return a + b; }, 0);
+  this.result = sum / this.prices.length;
+  this.age++;
+}
+
+module.exports = Indicator;
