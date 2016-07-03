2b1159b77788fd4abe12a184a7d94e97358f0fb8
Mike van Rossum <mike@mikevanrossum.nl>
2016-07-03 13:33:16 +0200

    awesome quotes!

--- gekko.js
+++ gekko.js
@@ -38,6 +38,8 @@ if(
 log.info('Gekko v' + util.getVersion(), 'started');
 log.info('I\'m gonna make you rich, Bud Fox.', '\n\n');
 
+// > Ever wonder why fund managers can't beat the S&P 500?
+// > 'Cause they're sheep, and sheep get slaughtered.
 pipeline({
   config: config,
   mode: mode
