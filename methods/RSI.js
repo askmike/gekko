a3b7ceff75d1eaf28d070fa74d787867819de6a3
Niklas Buschmann <niklasbuschmann@keybase.io>
2016-10-03 14:24:21 +0200

    match RSI to other indicators

--- methods/RSI.js
+++ methods/RSI.js
@@ -31,7 +31,7 @@ method.init = function() {
   this.requiredHistory = config.tradingAdvisor.historySize;
 
   // define the indicators we need
-  this.addIndicator('rsi', 'RSI', settings.interval);
+  this.addIndicator('rsi', 'RSI', settings);
 }
 
 // for debugging purposes log the last 
