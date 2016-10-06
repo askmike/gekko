68213730c686b7693b7af5c8b2c0bc9360397262
Roberto Rodriguez <prfiredragon@gmail.com>
2016-10-06 14:36:51 -0400

    use sample-config on testing (#3)

--- test/_prepare.js
+++ test/_prepare.js
@@ -1,4 +1,6 @@
+// overwrite config with sample-config
+
 var utils = require(__dirname + '/../core/util');
-var config = utils.getConfig();
-config.debug = false;
-utils.setConfig(config);
+var testConfig = require(__dirname + '/../sample-config.js');
+testConfig.debug = false;
+utils.setConfig(testConfig);
