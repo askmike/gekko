584874253a1fe35c6292051e0c5af90f4c034eea
Mike van Rossum <mike@mikevanrossum.nl>
2016-10-09 23:20:00 +0100

    cleanup util

--- core/util.js
+++ core/util.js
@@ -60,31 +60,6 @@ var util = {
   equals: function(a, b) {
     return !(a < b || a > b)
   },
-  msToMin: function(ms) {
-    return Math.round(ms / 60 / 1000);
-  },
-  minToMs: function(min) {
-    return min * 60 * 1000;
-  },
-  toMicro: function(moment) {
-    return moment.format('X') * 1000 * 1000;
-  },
-  intervalsAgo: function(amount) {
-    return moment().utc().subtract('minutes', config.EMA.interval * amount);
-  },
-  minAgo: function(moment) {
-    return moment.duration( moment().utc().subtract(moment) ).asMinutes();
-  },
-  average: function(list) {
-    var total = _.reduce(list, function(m, n) { return m + n }, 0);
-    return total / list.length;
-  },
-  calculateTimespan: function(a, b) {
-    if(a < b)
-      return b.diff(a);
-    else
-      return a.diff(b);
-  },
   defer: function(fn) {
     return function(args) {
       var args = _.toArray(arguments);
