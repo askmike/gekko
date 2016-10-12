3ef253f33f66038f86df23bce418110706429795
Mike van Rossum <mike@mikevanrossum.nl>
2016-10-12 11:54:37 +0100

    fix typos in pushbullet

--- plugins/pushbullet.js
+++ plugins/pushbullet.js
@@ -68,9 +68,9 @@ Pushbullet.prototype.processAdvice = function(advice) {
 Pushbullet.prototype.mail = function(subject, content, done) {
     var pusher = new pushbullet(pushbulletConfig.key);
     pusher.note(pushbulletConfig.email, subject, content, function(error, response) {
-        if(error || !resposne) {
+        if(error || !response) {
             log.error('Pushbullet ERROR:', error)
-        } else if(respone && response.active){
+        } else if(response && response.active){
             log.info('Pushbullet Message Sent')
         }
     });
