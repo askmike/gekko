47672f230d93cdd9694c1e7c9fe917a1e0f099bf
Mike van Rossum <mike@mikevanrossum.nl>
2016-10-30 01:14:10 +0000

    add backtest chart

--- web/vue/src/backtester/backtester.vue
+++ web/vue/src/backtester/backtester.vue
@@ -1,52 +1,50 @@
 <template lang='jade'>
   div
-    h2.px2.contain Run a backtest
+    h2.px2.contain Backtest
     .hr.contain
-    config-builder
+    config-builder(v-on:config='check')
+    div(v-if='backtestable')
+      .hr.contain
+      .txt--center
+        a.w100--s.my1.btn--blue(href='#', v-if='backtestState !== "fetching"', v-on:click.prevent='run') Backtest
+        p(v-if='backtestState === "fetching"').scan-btn Running backtest..
+    result(v-if='backtestResult', :result='backtestResult')
 </template>
 
 <script>
 import configBuilder from './configbuilder/configbuilder.vue'
+import result from './result/result.vue'
 
 export default {
   data: () => {
     return {
-      range: false,
-      dateRanges: [],
-      scanned: false,
+      backtestable: false,
+      backtestState: 'idle',
       backtestResult: false,
-      watchConfig: false,
+      config: false,
     }
   },
   methods: {
-    setRange: function(range) {
-      this.range = range;
+    check: function(config) {
+      this.config = config;
+
+      if(!config.valid)
+        return this.backtestable = false;
+
+      this.backtestable = true;
     },
-    onConfig: function(config) {
-      console.log(config.watch, this.watchConfig);
-      if(!_.isEqual(this.watchConfig, config.watch)) {
-        // market changed (or new)
-        this.scanned = false;
-        this.checkDateRange(config);
-        this.watchConfig = _.clone(config.watch);
-      }
-    },
-    checkDateRange: function(config) {
-      let to = 'http://localhost:3000/api/scan';
-      this.$http.post(to, config).then((response) => {
-        this.scanned = true;
-        this.dateRanges = response.body;
-      });
-    },
-    run: function(config) {
+    run: function() {
+      this.backtestState = 'fetching';
       let to = 'http://localhost:3000/api/backtest';
-      this.$http.post(to, config).then((response) => {
-        console.log(response);
+      this.$http.post(to, this.config).then(function(response) {
+        this.backtestState = 'fetched';
+        this.backtestResult = response.body;
       });
     }
   },
   components: {
-    configBuilder
+    configBuilder,
+    result
   }
 }
 </script>
