6ac4a61772a5f276ab7c60c6a85343e3a55f8211
Mike van Rossum <mike@mikevanrossum.nl>
2016-11-12 16:12:14 +0000

    make import a part of `Local Data`

---
+++ web/vue/src/data/data.vue
@@ -0,0 +1,23 @@
+<template lang='jade'>
+  div.contain.my2 blup!
+</template>
+
+<script>
+
+import marked from '../tools/marked'
+
+let intro = marked(`
+
+## Import data
+
+In order to backtest your strategies using Gekko against
+historical data you first need to import data into Gekko.
+
+`)
+
+export default {
+}
+</script>
+
+<style>
+</style>
