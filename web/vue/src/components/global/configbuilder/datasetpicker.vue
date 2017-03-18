866f0960ef48b11403054de9e5a5955eb8cbe7cc
Mike van Rossum <mike@mikevanrossum.nl>
2017-03-18 00:07:30 +0000

    allow daterange configuration in datasetpicker

--- web/vue/src/components/global/configbuilder/datasetpicker.vue
+++ web/vue/src/components/global/configbuilder/datasetpicker.vue
@@ -26,9 +26,21 @@ div
           td {{ fmt(set.from) }}
           td {{ fmt(set.to) }}
           td {{ humanizeDuration(set.to.diff(set.from)) }}
+    a(href='#', v-on:click.prevent='openRange', v-if='!rangeVisible') adjust range
+    template(v-if='rangeVisible')
+      div
+        label(for='customFrom') From:
+        input(v-model='customFrom')
+      div
+        label(for='customTo') To:
+        input(v-model='customTo')
+
 </template>
 
 <script>
+
+import _ from 'lodash'
+import Vue from 'vue'
 
 import { post } from '../../../tools/ajax'
 import spinner from '../../global/blockSpinner.vue'
@@ -40,7 +52,11 @@ export default {
   },
   data: () => {
     return {
-      setIndex: -1
+      setIndex: -1,
+      customTo: false,
+      customFrom: false,
+      rangeVisible: false,
+      set: false
     };
   },
   mixins: [ dataset ],
@@ -48,20 +64,51 @@ export default {
     humanizeDuration: (n) => {
       return window.humanizeDuration(n, {largest: 4});
     },
-    fmt: mom => mom.format('YYYY-MM-DD HH:mm')
-  },
-  computed: {
-    set: function() {
-      return this.datasets[this.setIndex];
-    }
-  },
-  watch: {
-    set: function(val) {
+    fmt: mom => mom.format('DD-MM-YYYY HH:mm'),
+    openRange: function() {
+      if(this.setIndex === -1)
+        return alert('select a range first');
+
+      this.rangeVisible = true;
+
+      if(!this.customTo)
+        this.updateCustomRange();
+    },
+    updateCustomRange: function() {
+      this.customTo = this.fmt(this.set.to);
+      this.customFrom = this.fmt(this.set.from);
+    },
+    emitSet: function(val) {
       if(!val)
         return;
 
-      this.$emit('dataset', val);
+      let set;
+
+      if(!this.customTo)
+        set = val;
+      else {
+        set = Vue.util.extend({}, val);
+        set.to = moment(this.customTo);
+        set.from = moment(this.customFrom);
+      }
+
+      console.log(set.to, set.from);
+
+      this.$emit('dataset', set);
     }
+  },
+  watch: {
+
+    setIndex: function() {
+      this.set = this.datasets[this.setIndex];
+
+      this.updateCustomRange();
+
+      this.emitSet(this.set);
+    },
+
+    customTo: function() { this.emitSet(this.set); },
+    customFrom: function() { this.emitSet(this.set); }
   }
 }
 </script>
