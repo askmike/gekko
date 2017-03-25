265104d34268294b75ff82c960edbd7a9af2f2a5
Mike van Rossum <mike@mikevanrossum.nl>
2017-03-25 13:33:47 +0000

    normalize time fmt to iso 8601

--- plugins/profitSimulator.js
+++ plugins/profitSimulator.js
@@ -218,8 +218,8 @@ Logger.prototype.calculateReportStatistics = function() {
     currency: this.currency,
     asset: this.asset,
 
-    startTime: this.dates.start.utc().format('DD-MM-YYYY HH:mm:ss'),
-    endTime: this.dates.end.utc().format('DD-MM-YYYY HH:mm:ss'),
+    startTime: this.dates.start.utc().format('YYYY-MM-DD HH:mm:ss'),
+    endTime: this.dates.end.utc().format('YYYY-MM-DD HH:mm:ss'),
     timespan: timespan,
     buynhold: this.endPrice * 100 / this.startPrice - 100,
     
