8bb7bf5266012577e0fda4c57f6e3a9c83316911
Woo Jin <woog2nius@gmail.com>
2017-08-07 03:41:05 +0900

    remove 80 to resolve ssl issue

--- web/vue/src/tools/api.js
+++ web/vue/src/tools/api.js
@@ -1,7 +1,7 @@
 // global window.CONFIG
 
 const config = window.CONFIG.ui;
-const host = `${config.host}:${config.port}${config.path}api/`;
+const host = `${config.host}${config.port === 80 ? '' : `:${config.port}`}${config.path}api/`;
 
 // rest API path
 if(config.ssl) {
@@ -16,4 +16,5 @@ if(config.ssl) {
 } else {
   var wsPath = `ws://${host}`;
 }
+
 export {wsPath,restPath};
