a6f1639d1919b7d75a4f0a036787c7347f73eb67
generalectric <jmbeck210@gmail.com>
2017-10-12 20:01:07 -0500

    added default to WAL

--- plugins/sqlite/handle.js
+++ plugins/sqlite/handle.js
@@ -50,6 +50,6 @@ if(mode === 'realtime' || mode === 'importer') {
 }
 
 var db = new sqlite3.Database(fullPath);
-db.run('PRAGMA journal_mode = ' + config.sqlite.journalMode);
+db.run('PRAGMA journal_mode = ' + config.sqlite.journalMode||'WAL');
 
 module.exports = db;
