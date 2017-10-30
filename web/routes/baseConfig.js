aa3c89f6f8ddc11a17879cefc8292ab2b89bc92b
generalectric <jmbeck210@gmail.com>
2017-10-30 17:12:27 -0500

    add schema to postgresql config

--- web/routes/baseConfig.js
+++ web/routes/baseConfig.js
@@ -57,6 +57,7 @@ config.postgresql = {
   version: 0.1,
   connectionString: 'postgres://user:pass@localhost:5432', // if default port
   database: null, // if set, we'll put all tables into a single database.
+  schema: 'public',
   dependencies: [{
     module: 'pg',
     version: '6.1.0'
