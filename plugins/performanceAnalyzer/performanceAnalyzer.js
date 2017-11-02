556c57d42c4704d63a344e4023186e7da978efdb
Matt <matt@gauzza.com>
2017-11-01 22:49:25 -0400

    Fixing calculation issues with roundtrip table
    Changing this.price to trade.price and this.current to trade.portfolio.
     stable

--- plugins/performanceAnalyzer/performanceAnalyzer.js
+++ plugins/performanceAnalyzer/performanceAnalyzer.js
@@ -81,14 +81,14 @@ PerformanceAnalyzer.prototype.logRoundtripPart = function(trade) {
   if(trade.action === 'buy') {
     this.roundTrip.entry = {
       date: trade.date,
-      price: this.price,
-      total: this.current.asset * this.price,
+      price: trade.price,
+      total: trade.portfolio.asset * trade.price,
     }
   } else if(trade.action === 'sell') {
     this.roundTrip.exit = {
       date: trade.date,
-      price: this.price,
-      total: this.current.currency
+      price: trade.price,
+      total: trade.portfolio.currency
     }
 
     this.handleRoundtrip();
@@ -100,7 +100,7 @@ PerformanceAnalyzer.prototype.round = function(amount) {
 }
 
 PerformanceAnalyzer.prototype.handleRoundtrip = function() {
-  const roundtrip = {
+  var roundtrip = {
     entryAt: this.roundTrip.entry.date,
     entryPrice: this.roundTrip.entry.price,
     entryBalance: this.roundTrip.entry.total,
