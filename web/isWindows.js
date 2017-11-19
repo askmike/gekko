245dd2202da868fc97ed8f49db1cf4015b6e61a8
Clifford Roche <clifford.roche@gmail.com>
2017-11-19 15:40:43 -0500

    Fix windows checker...

--- web/isWindows.js
+++ web/isWindows.js
@@ -2,7 +2,7 @@ const os = require('os');
 
 var isWindows = (
     os.platform() == 'win32' // true evenon 64 bit archs
-    || os.release().substr('Microsoft') > -1 // bash on Windows 10 scenario
+    || os.release().indexOf('Microsoft') > -1 // bash on Windows 10 scenario
 );
 
 module.exports = isWindows;
