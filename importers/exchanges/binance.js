06484ae88fca68c3f7e456edb469a83c53eca988
Clifford Roche <clifford.roche@gmail.com>
2017-11-24 21:10:39 -0500

    Fix import for days with no records

--- importers/exchanges/binance.js
+++ importers/exchanges/binance.js
@@ -17,42 +17,41 @@ var from = false;
 var fetcher = new Fetcher(config.watch);
 
 var fetch = () => {
-    fetcher.import = true;
-    fetcher.getTrades(from, handleFetch);
-}
+  fetcher.import = true;
+  fetcher.getTrades(from, handleFetch);
+};
 
 var handleFetch = (unk, trades) => {
+  if (trades.length > 0) {
     var last = moment.unix(_.last(trades).date);
 
-    if(last < from) {
-        log.debug('Skipping data, they are before from date (probably a programming error)', last.format());
-        return fetch();
+    if (last < from) {
+      log.debug(
+        'Skipping data, they are before from date (probably a programming error)',
+        last.format()
+      );
+      return fetch();
     }
+  }
 
-    var next = from.add(1, 'd');
-    if  (next >= end) {
-        fetcher.emit('done');
+  var next = from.add(1, 'd');
+  if (next >= end) {
+    fetcher.emit('done');
 
-        var endUnix = end.unix();
-        trades = _.filter(
-            trades,
-            t => t.date <= endUnix
-        )
-    }
+    var endUnix = end.unix();
+    trades = _.filter(trades, t => t.date <= endUnix);
+  }
 
-    from = next.clone();
-    fetcher.emit('trades', trades);
-}
+  from = next.clone();
+  fetcher.emit('trades', trades);
+};
 
-module.exports = function (daterange) {
+module.exports = function(daterange) {
+  from = daterange.from.clone();
+  end = daterange.to.clone();
 
-    from = daterange.from.clone();
-    end = daterange.to.clone();
-
-    return {
-        bus: fetcher,
-        fetch: fetch
-    }
-}
-
-
+  return {
+    bus: fetcher,
+    fetch: fetch,
+  };
+};
