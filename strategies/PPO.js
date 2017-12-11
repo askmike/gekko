ffd06e237fecdd13706bd91c89917c07ac9e83dd
generalectric <jmbeck210@gmail.com>
2017-12-10 22:05:56 -0600

    ppo output to this.result for api exposure (#1443)
    * fix ppo result for api exposure
    
    * fix tests
    
    * fix tests

--- strategies/PPO.js
+++ strategies/PPO.js
@@ -40,12 +40,12 @@ method.update = function(candle) {
 method.log = function() {
   var digits = 8;
   var ppo = this.indicators.ppo;
-  var short = ppo.short.result;
-  var long = ppo.long.result;
-  var macd = ppo.macd;
-  var result = ppo.ppo;
-  var macdSignal = ppo.MACDsignal.result;
-  var ppoSignal = ppo.PPOsignal.result;
+  var long = ppo.result.longEMA;
+  var short = ppo.result.shortEMA;
+  var macd = ppo.result.macd;
+  var result = ppo.result.ppo;
+  var macdSignal = ppo.result.MACDsignal;
+  var ppoSignal = ppo.result.PPOsignal;
 
   log.debug('calculated MACD properties for candle:');
   log.debug('\t', 'short:', short.toFixed(digits));
@@ -62,12 +62,12 @@ method.check = function(candle) {
   var price = candle.close;
 
   var ppo = this.indicators.ppo;
-  var long = ppo.long.result;
-  var short = ppo.short.result;
-  var macd = ppo.macd;
-  var result = ppo.ppo;
-  var macdSignal = ppo.MACDsignal.result;
-  var ppoSignal = ppo.PPOsignal.result;
+  var long = ppo.result.longEMA;
+  var short = ppo.result.shortEMA;
+  var macd = ppo.result.macd;
+  var result = ppo.result.ppo;
+  var macdSignal = ppo.result.MACDsignal;
+  var ppoSignal = ppo.result.PPOsignal;
 
   // TODO: is this part of the indicator or not?
   // if it is it should move there
