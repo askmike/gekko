a9fdddd006034d3e9a81b2c7b77913d173e85223
Ozan HACIBEKİROĞLU <ozan_haci@yahoo.com>
2018-01-05 21:35:38 +0300

    fix email options bug (#1521)
    * fix email options bug

--- core/util.js
+++ core/util.js
@@ -177,7 +177,7 @@ var util = {
     return startTime;
   },
   retry: function(fn, callback) {
-    var operation = {
+    var options = {
       retries: 5,
       factor: 1.2,
       minTimeout: 1 * 1000,
