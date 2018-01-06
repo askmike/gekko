cbae18a75ba428812d0900a1f34d3d5d0fe78b1c
Clifford Roche <clifford.roche@gmail.com>
2018-01-06 15:32:15 -0500

    Compensate for possible leap seconds in API query
    Who thought this would ever have to be considered...

--- importers/exchanges/binance.js
+++ importers/exchanges/binance.js
@@ -30,9 +30,11 @@ var handleFetch = (err, trades) => {
 
   if (trades.length > 0) {
     var last = moment.unix(_.last(trades).date).utc();
-    var next = from.clone().add(1, 'h');
+    // Conversion to milliseconds epoch time means we have to compensate for possible leap seconds
+    var next = from.clone().add(1, 'h').subtract(1, 's');
   } else {
-    var next = from.clone().add(1, 'h');
+    // Conversion to milliseconds epoch time means we have to compensate for possible leap seconds
+    var next = from.clone().add(1, 'h').subtract(1, 's');
     log.debug('Import step returned no results, moving to the next 1h period');
   }
 
