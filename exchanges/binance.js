d0f94cbec5106773d7a78f64e5d7474af41c2bcf
Richard King <richard.king@rocketspark.com>
2018-02-03 23:53:30 +1300

    Removed DEMA script & added space after //

--- exchanges/binance.js
+++ exchanges/binance.js
@@ -256,8 +256,8 @@ Trader.prototype.getOrder = function(order, callback) {
     var price = parseFloat(data.price);
     var amount = parseFloat(data.executedQty);
     
-    //Data.time is a 13 digit millisecon unix time stamp.
-    //https://momentjs.com/docs/#/parsing/unix-timestamp-milliseconds/ 
+    // Data.time is a 13 digit millisecon unix time stamp.
+    // https://momentjs.com/docs/#/parsing/unix-timestamp-milliseconds/ 
     var date = moment(data.time);
 
     callback(undefined, { price, amount, date });
