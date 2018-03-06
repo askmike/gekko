36f361210cc941b68af1579dffc173ef67bc1139
Aidas Klimas <aidaskk@gmail.com>
2018-03-06 21:26:19 +0200

    telegrambot refactor
    new features:
    Supports multible subscribers cia /subscribe and /unsubscribe commands
    Suports custom /donate text

--- plugins/telegrambot.js
+++ plugins/telegrambot.js
@@ -1,35 +1,37 @@
-var log = require('../core/log');
-var moment = require('moment');
-var _ = require('lodash');
-var config = require('../core/util').getConfig();
-var telegrambot = config.telegrambot;
-var utc = moment.utc;
+const log = require('../core/log');
+const moment = require('moment');
+const _ = require('lodash');
+const config = require('../core/util').getConfig();
+const telegrambot = config.telegrambot;
+const utc = moment.utc;
+const telegram = require("node-telegram-bot-api");
 
-var telegram = require("node-telegram-bot-api");
-
-var Actor = function() {
+const Actor = function() {
   _.bindAll(this);
 
-  this.advice = 'Dont got one yet :(';
+  this.advice = null;
   this.adviceTime = utc();
 
   this.price = 'Dont know yet :(';
   this.priceTime = utc();
 
   this.commands = {
+    '/start': 'emitStart',
     '/advice': 'emitAdvice',
+    '/subscribe': 'emitSubscribe',
+    '/unsubscribe': 'emitUnSubscribe',
     '/price': 'emitPrice',
-    '/donate': 'emitDonation',
-    '/realadvice': 'emitRealAdvice', //Without space
     '/help': 'emitHelp'
   };
-
+  if (telegrambot.donate) {
+    this.commands['/donate'] = 'emitDonate';
+  }
   this.rawCommands = _.keys(this.commands);
-
   this.chatId = null;
+  this.subscribers = [];
   this.bot = new telegram(telegrambot.token, { polling: true });
   this.bot.onText(/(.+)/, this.verifyQuestion);
-}
+};
 
 Actor.prototype.processCandle = function(candle, done) {
   this.price = candle.close;
@@ -39,32 +41,46 @@ Actor.prototype.processCandle = function(candle, done) {
 };
 
 Actor.prototype.processAdvice = function(advice) {
-  if (telegrambot.muteSoft && advice.recommendation === 'soft') return;
+  if (advice.recommendation === 'soft') return;
   this.advice = advice.recommendation;
   this.adviceTime = utc();
-
-  if (telegrambot.emitUpdates)
-    this.newAdvice();
+  this.advicePrice = this.price;
+  this.subscribers.forEach(this.emitAdvice, this);
 };
 
 Actor.prototype.verifyQuestion = function(msg, text) {
   this.chatId = msg.chat.id;
-  if (text[1].toLowerCase() in this.commands)
+  if (text[1].toLowerCase() in this.commands) {
     this[this.commands[text[1].toLowerCase()]]();
-  else
-    this.bot.sendMessage(this.chatId, "Hello!");
-}
-
-Actor.prototype.newAdvice = function() {
-  if (this.chatId) {
-    this.bot.sendMessage(this.chatId, 'Important news!');
-    this.emitAdvice();
+  } else {
+    this.emitHelp();
   }
-}
+};
 
-// sent advice to the last chat
-Actor.prototype.emitAdvice = function() {
-  var message = [
+Actor.prototype.emitStart = function() {
+  this.bot.sendMessage(this.chatId, 'Hello! How can I help you?');
+};
+
+Actor.prototype.emitSubscribe = function() {
+  if (this.subscribers.indexOf(this.chatId) === -1) {
+    this.subscribers.push(this.chatId);
+    this.bot.sendMessage(this.chatId, `Success! Got ${this.subscribers.length} subscribers.`);
+  } else {
+    this.bot.sendMessage(this.chatId, "You are already subscribed.");
+  }
+};
+
+Actor.prototype.emitUnSubscribe = function() {
+  if (this.subscribers.indexOf(this.chatId) > -1) {
+    this.subscribers.splice(this.subscribers.indexOf(this.chatId), 1);
+    this.bot.sendMessage(this.chatId, "Success!");
+  } else {
+    this.bot.sendMessage(this.chatId, "You are not subscribed.");
+  }
+};
+
+Actor.prototype.emitAdvice = function(chatId) {
+  let message = [
     'Advice for ',
     config.watch.exchange,
     ' ',
@@ -76,21 +92,30 @@ Actor.prototype.emitAdvice = function() {
     ' at ',
     config.tradingAdvisor.candleSize,
     ' minute candles, is:\n',
-    this.advice,
-    ' ',
-    config.watch.asset,
-    ' (from ',
-      this.adviceTime.fromNow(),
-    ')'
   ].join('');
+  if (this.advice) {
+    message += this.advice +
+      ' ' +
+      config.watch.asset +
+      ' ' +
+      this.advicePrice +
+      ' (' +
+      this.adviceTime.fromNow() +
+      ')';
+  } else {
+    message += 'None'
+  }
 
-  if (this.chatId)
+  if (chatId) {
+    this.bot.sendMessage(chatId, message);
+  } else {
     this.bot.sendMessage(this.chatId, message);
+  }
 };
 
 // sent price over to the last chat
 Actor.prototype.emitPrice = function() {
-  var message = [
+  const message = [
     'Current price at ',
     config.watch.exchange,
     ' ',
@@ -102,54 +127,28 @@ Actor.prototype.emitPrice = function() {
     ' ',
     config.watch.currency,
     ' (from ',
-      this.priceTime.fromNow(),
+    this.priceTime.fromNow(),
     ')'
   ].join('');
 
-  if (this.chatId)
-    this.bot.sendMessage(this.chatId, message);
+  this.bot.sendMessage(this.chatId, message);
 };
 
-// sent donation info over to the IRC channel
-Actor.prototype.emitDonation = function() {
-  var message = 'You want to donate? How nice of you! You can send your coins here:';
-  message += '\nBTC:\t13r1jyivitShUiv9FJvjLH7Nh1ZZptumwW';
-
-  if (this.chatId)
-    this.bot.sendMessage(this.chatId, message);
+Actor.prototype.emitDonate = function() {
+  this.bot.sendMessage(this.chatId, telegrambot.donate);
 };
 
 Actor.prototype.emitHelp = function() {
-  var message = _.reduce(
+  let message = _.reduce(
     this.rawCommands,
     function(message, command) {
       return message + ' ' + command + ',';
     },
-    'possible commands are:'
+    'Possible commands are:'
   );
-
   message = message.substr(0, _.size(message) - 1) + '.';
-
-  if (this.chatId)
-    this.bot.sendMessage(this.chatId, message);
-}
-
-Actor.prototype.emitRealAdvice = function() {
-  // http://www.examiner.com/article/uncaged-a-look-at-the-top-10-quotes-of-gordon-gekko
-  // http://elitedaily.com/money/memorable-gordon-gekko-quotes/
-  var realAdvice = [
-    'I don\'t throw darts at a board. I bet on sure things. Read Sun-tzu, The Art of War. Every battle is won before it is ever fought.',
-    'Ever wonder why fund managers can\'t beat the S&P 500? \'Cause they\'re sheep, and sheep get slaughtered.',
-    'If you\'re not inside, you\'re outside!',
-    'The most valuable commodity I know of is information.',
-    'It\'s not a question of enough, pal. It\'s a zero sum game, somebody wins, somebody loses. Money itself isn\'t lost or made, it\'s simply transferred from one perception to another.',
-    'What\'s worth doing is worth doing for money. (Wait, wasn\'t I a free and open source bot?)',
-    'When I get a hold of the son of a bitch who leaked this, I\'m gonna tear his eyeballs out and I\'m gonna suck his fucking skull.'
-  ];
-
-  if (this.chatId)
-    this.bot.sendMessage(this.chatId, _.first(_.shuffle(realAdvice)));
-}
+  this.bot.sendMessage(this.chatId, message);
+};
 
 Actor.prototype.logError = function(message) {
   log.error('Telegram ERROR:', message);
--- sample-config.js
+++ sample-config.js
@@ -302,10 +302,8 @@ config.ircbot = {
 
 config.telegrambot = {
   enabled: false,
-  emitUpdates: false,
   token: 'YOUR_TELEGRAM_BOT_TOKEN',
-  botName: 'gekkobot'
-}
+};
 
 config.twitter = {
     // sends pushbullets if true
