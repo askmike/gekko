065ca16410dac15ff8b82794bc89937243557253
Mike van Rossum <mike@mikevanrossum.nl>
2018-04-17 17:42:45 +0700

    add limit logic to sticky

--- exchange/orders/sticky.js
+++ exchange/orders/sticky.js
@@ -1,8 +1,8 @@
 /*
   The sticky order is an advanced order:
     - It is created at max price X
-      - if max is not specified always at bbo.
-      - if max is specified the price is either max or the bbo (whichever is comes first)
+      - if limit is not specified always at bbo.
+      - if limit is specified the price is either limit or the bbo (whichever is comes first)
     - it will readjust the order:
       - if overtake is true it will overbid the current bbo <- TODO
       - if overtake is false it will stick to current bbo when this moves
@@ -28,7 +28,7 @@ class StickyOrder extends BaseOrder {
     super(api);
   }
 
-  create(side, rawAmount, params) {
+  create(side, rawAmount, params = {}) {
     this.side = side;
 
     this.amount = this.api.roundAmount(rawAmount);
@@ -36,14 +36,26 @@ class StickyOrder extends BaseOrder {
     if(this.amount < this.data.market.minimalOrder.amount)
       throw new Error('Amount is too small');
 
+    if(side === 'buy') {
+      if(params.limit)
+        this.limit = this.api.roundPrice(params.limit);
+      else
+        this.limit = Infinity;
+    } else {
+      if(params.limit)
+        this.limit = this.api.roundPrice(params.limit);
+      else
+        this.limit = -Infinity;
+    }
+
     this.status = states.SUBMITTED;
     this.emitStatus();
 
-    // note: currently always sticks to BBO, does not overtake
+    // note: currently always sticks to max BBO, does not overtake
     if(side === 'buy')
-      this.price = this.data.ticker.bid;
+      this.price = Math.min(this.data.ticker.bid, this.limit);
     else
-      this.price = this.data.ticker.ask;
+      this.price = Math.max(this.data.ticker.ask, this.limit);
 
     this.submit();  
 
@@ -75,12 +87,17 @@ class StickyOrder extends BaseOrder {
       if(filled)
         return this.filled(this.price);
 
+      // if we are already at limit we dont care where the top is
+      // note: might be string VS float
+      if(this.price == this.limit)
+        return setTimeout(this.checkOrder, this.checkInterval);
+
       this.api.getTicker((err, ticker) => {
         let top;
         if(this.side === 'buy')
-          top = ticker.bid;
+          top = Math.min(ticker.bid, this.limit);
         else
-          top = ticker.ask;
+          top = Math.max(ticker.ask, this.limit);
 
         // note: might be string VS float
         if(top != this.price)
