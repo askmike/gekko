1e1dc69eab2bfff454d9b70cb56a7cee16e7ae1c
Mike van Rossum <mike@mikevanrossum.nl>
2018-04-21 21:59:09 +0700

    add generic submit to order

--- exchange/orders/order.js
+++ exchange/orders/order.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const _ = require('lodash');
 
 const bindAll = require('../exchangeUtils').bindAll;
 const states = require('./states');
@@ -11,12 +12,63 @@ class BaseOrder extends EventEmitter {
 
     this.api = api;
 
-    this.checkInterval = 1000;
+    this.checkInterval = 1500;
 
     this.status = states.INITIALIZING;
-    this.emitStatus();
+
+    this.completed = false;
 
     bindAll(this);
+  }
+
+  submit({side, amount, price, alreadyFilled}) {
+
+    // Check amount
+    if(amount < this.data.market.minimalOrder.amount) {
+      if(alreadyFilled) {
+        // partially filled, but the remainder is too
+        // small.
+        return this.filled();
+      }
+
+      // We are not partially filled, meaning the
+      // amount passed was too small to even start.
+      throw new Error('Amount is too small');
+    }
+
+    // Some exchanges have restrictions on prices
+    if(
+      _.isFunction(this.api.isValidPrice) &&
+      !this.api.isValidPrice(price)
+    ) {
+      if(alreadyFilled) {
+        // partially filled, but the remainder is too
+        // small.
+        return this.filled();
+      }
+
+      // We are not partially filled, meaning the
+      // amount passed was too small to even start.
+      throw new Error('Price is not valid');
+    }
+
+    // Some exchanges have restrictions on lot sizes
+    if(
+      _.isFunction(this.api.isValidLot) &&
+      !this.api.isValidLot(this.price, amount)
+    ) {
+      if(alreadyFilled) {
+        // partially filled, but the remainder is too
+        // small.
+        return this.filled();
+      }
+
+      // We are not partially filled, meaning the
+      // amount passed was too small to even start.
+      throw new Error('Lot size is too small');
+    }
+
+    this.api[this.side](amount, this.price, this.handleCreate);
   }
 
   setData(data) {
@@ -27,12 +79,23 @@ class BaseOrder extends EventEmitter {
     this.emit('statusChange', this.status);
   }
 
+  cancelled() {
+    this.status = states.CANCELLED;
+    this.completed = true;
+    this.finish();
+  }
+
+  rejected(reason) {
+    this.rejectedReason = reason;
+    this.status = states.REJECTED;
+    this.finish();
+  }
+
   filled(price) {
     this.status = states.FILLED;
     this.emitStatus();
 
-    this.status = states.COMPLETED;
-    this.emitStatus();
+    this.completed = true;
 
     this.emit('filled', {
       id: this.id,
@@ -44,8 +107,9 @@ class BaseOrder extends EventEmitter {
   }
 
   finish(filled) {
+    this.completed = true;
     this.emit('completed', {
-      id: this.id,
+      status: this.status,
       filled
     })
   }
