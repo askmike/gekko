b546c4328241ee2dc5f3043a40ce92d93257d71c
Mike van Rossum <mike@mikevanrossum.nl>
2018-07-06 01:10:50 +0800

    disable mail retry, temp fix #2275. Revert "Retry sending mail."
    This reverts commit 61f44e5378b20ff37b3ea1e0b2845b653f74205c.

--- plugins/mailer.js
+++ plugins/mailer.js
@@ -81,14 +81,13 @@ Mailer.prototype.setup = function(done) {
 };
 
 Mailer.prototype.mail = function(subject, content, done) {
-  util.retry(function(cb) {
-    this.server.send({
-      text: content,
-      from: mailConfig.from,
-      to: mailConfig.to,
-      subject: mailConfig.tag + subject
-    }, cb);
-  }.bind(this), done || this.checkResults);
+
+  this.server.send({
+    text: content,
+    from: mailConfig.from,
+    to: mailConfig.to,
+    subject: mailConfig.tag + subject
+  }, done || this.checkResults);
 };
 
 Mailer.prototype.processCandle = function(candle, done) {
