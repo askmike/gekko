c009f3513de49f0789ef1ee84379c090e7128911
Mike van Rossum <mike@mikevanrossum.nl>
2018-07-16 18:39:25 +0800

    normalize code to gekko's codebase

--- plugins/postgresql/handle.js
+++ plugins/postgresql/handle.js
@@ -15,16 +15,14 @@ const pluginHelper = require(dirs.core + 'pluginUtil');
 const pluginMock = {
   slug: 'postgresql adapter',
   dependencies: config.postgresql.dependencies
-};
+}
 
 const cannotLoad = pluginHelper.cannotLoad(pluginMock);
-if(cannotLoad){
+if(cannotLoad) {
   util.die(cannotLoad);
 }
 
 const pg = require('pg');
-
-const plugins = require(util.dirs().gekko + 'plugins');
 
 const version = adapter.version;
 
@@ -35,59 +33,58 @@ const mode = util.gekkoMode();
 const connectionString = config.postgresql.connectionString;
 
 const checkClient = new pg.Pool({
-  connectionString: connectionString+'/postgres',
+  connectionString: connectionString + '/postgres',
 });
 const pool = new pg.Pool({
-  connectionString: connectionString+'/'+dbName,
+  connectionString: connectionString + '/' + dbName,
 });
 
-/* Postgres does not have 'create database if not exists' so we need to check if the db exists first.
-This requires connecting to the default postgres database first. Your postgres user will need appropriate rights. */
-//checkClient.connect(function(err){
-  //if(err){
-    //util.die(err);
-  //}
-checkClient.connect((err, client, done) => {  
-  log.debug("Check database exists: "+dbName);
+// We need to check if the db exists first.
+// This requires connecting to the default
+// postgres database first. Your postgres
+// user will need appropriate rights.
+checkClient.connect((err, client, done) => {
+  log.debug("Check database exists: " + dbName);
   const query = client.query("select count(*) from pg_catalog.pg_database where datname = $1",[dbName], 
     (err, res) => {
       if(err) {
-        done();
         util.die(err);
       }
-      if(res.rows[0].count == 0) { //database does not exist
-        log.debug("Database "+dbName+" does not exist");
-        if(mode === 'realtime') { //create database if not found
-          log.debug("Creating database "+dbName);
-          client.query("CREATE DATABASE "+dbName,function(err) {
-            done();
-            if(err){
-              done();
-              util.die(err);
-            } else {
-                log.debug("Postgres connection pool is ready, db "+dbName);
-                upsertTables();
-            }
-          });
-        }else if(mode === 'backtest') {
-          done();
-          util.die(`History does not exist for exchange ${config.watch.exchange}.`);
-        }else{
-          done();
-          util.die(`Start gekko first in realtime mode to create tables. You are currently in the '${mode}' mode.`);
-        }
-      }else{ //database exists
-        done();
-        log.debug("Database exists: "+dbName);
-        log.debug("Postgres connection pool is ready, db "+dbName);
-        upsertTables();
-      }  
+
+      if(res.rows[0].count !== 0) {
+        // database exists
+        log.debug("Database exists: " + dbName);
+        log.debug("Postgres connection pool is ready, db " + dbName);
+        upsertTables(done);
+        return;
+      }
+
+      // database dot NOT exist
+
+      if(mode === 'backtest') {
+        // no point in trying to backtest with
+        // non existing data.
+        util.die(`History does not exist for exchange ${config.watch.exchange}.`);
+      }
+
+      createDatabase(done);
     });
 });
 
 
-function upsertTables() {
-  var upsertQuery = 
+const createDatabase = next => {
+  client.query("CREATE DATABASE " + dbName, err => {
+    if(err) {
+      util.die(err);
+    }
+
+    log.debug("Postgres connection pool is ready, db " + dbName);
+    upsertTables(next);
+  });
+}
+
+const upsertTables = next => {
+  const upsertQuery =
     `CREATE TABLE IF NOT EXISTS
     ${postgresUtil.table('candles')} (
       id BIGSERIAL PRIMARY KEY,
@@ -102,9 +99,12 @@ function upsertTables() {
     );`;
 
 
-  pool.connect((err,client,done) => {
+  pool.connect((err, client, done) => {
+    if(err) {
+      util.die(err);
+    }
     client.query(upsertQuery, (err) => {
-      done();
+      next();
     });
   });
 }
