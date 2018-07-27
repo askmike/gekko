157a99fa7fc95107ec40e1996e3a44899fad083e
Jack Crypto49er <39413731+crypto49er@users.noreply.github.com>
2018-07-27 11:12:06 -0400

    Changed several log messages from debug to warn (#2374)
    Changed "Not creating order" message and "exchange did not provide fee info" from debug to warn messages so users can see these issues even when debug is set to false.

--- plugins/trader/trader.js
+++ plugins/trader/trader.js
@@ -219,7 +219,7 @@ Trader.prototype.createOrder = function(side, amount, advice, id) {
   const check = this.broker.isValidOrder(amount, this.price);
 
   if(!check.valid) {
-    log.debug('NOT creating order! Reason:', check.reason);
+    log.warn('NOT creating order! Reason:', check.reason);
     return this.deferredEmit('tradeAborted', {
       id,
       adviceId: advice.id,
@@ -278,7 +278,7 @@ Trader.prototype.createOrder = function(side, amount, advice, id) {
             effectivePrice = summary.price * (1 - summary.feePercent / 100);
           }
         } else {
-          log.debug('WARNING: exchange did not provide fee information, assuming no fees..');
+          log.warn('WARNING: exchange did not provide fee information, assuming no fees..');
           effectivePrice = summary.price;
         }
 
