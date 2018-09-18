767f225feec75ce0158b6d08253a68a6bcd4ff68
Mike van Rossum <mike@mikevanrossum.nl>
2018-09-18 11:16:37 +0800

    manually trigger gekko stream finalize

--- core/markets/backtest.js
+++ core/markets/backtest.js
@@ -74,6 +74,7 @@ Market.prototype.processCandles = function(err, candles) {
     if(this.ended) {
       this.closed = true;
       this.reader.close();
+      this.push({isFinished: true});
     } else {
       util.die('Query returned no candles (do you have local data for the specified range?)');
     }
