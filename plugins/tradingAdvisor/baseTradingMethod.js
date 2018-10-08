3a49d630c3880ebe3f8b81e9d8b363755a3d5eb3
Evgeny <eusorov@gmail.com>
2018-10-08 07:58:49 +0200

    bugfix #2548 if no strategy.historySize found, than get it from tradingAdvisor (#2577)
    * bugfix #2548 if no strategy.historySize found, get  from tradingAdvisor
    
    * check with _.isNumber

--- plugins/tradingAdvisor/baseTradingMethod.js
+++ plugins/tradingAdvisor/baseTradingMethod.js
@@ -37,7 +37,6 @@ var Base = function(settings) {
   this.settings = settings;
   this.tradingAdvisor = config.tradingAdvisor;
   // defaults
-  this.requiredHistory = 0;
   this.priceValue = 'close';
   this.indicators = {};
   this.asyncTick = false;
@@ -68,6 +67,11 @@ var Base = function(settings) {
 
   // let's run the implemented starting point
   this.init();
+
+  //if no requiredHistory was provided, set default from tradingAdvisor
+  if (!_.isNumber(this.requiredHistory)){
+    this.requiredHistory = config.tradingAdvisor.historySize;
+  }
 
   if(!config.debug || !this.log)
     this.log = function() {};
