25bb888f175e4eb1fa991c043a1fa2bf5cc428b0
Mike van Rossum <mike@mikevanrossum.nl>
2019-01-07 21:14:04 +0800

    [plugin] batch candle uploads

--- plugins/candleUploader.js
+++ plugins/candleUploader.js
@@ -6,30 +6,54 @@ const config = util.getConfig();
 
 const CandleUploader = function(done) {
   _.bindAll(this);
+
   done();
+  this.candles = [];
+  this.schedule();
 };
 
 CandleUploader.prototype.processCandle = function(candle, done) {
-  console.log(new Date, 'uploading', candle.start.format());
+  this.candles.push(candle);
+  done();
+};
+
+CandleUploader.prototype.schedule = function() {
+  this.timer = setTimeout(this.upload, 10 * 1000);
+}
+
+CandleUploader.prototype.upload = function() {
+  const amount = this.candles.length;
+  if(!amount) {
+    return this.schedule();
+  }
+
   axios({
     url: config.candleUploader.url,
     method: 'post',
     data: {
       apiKey: config.candleUploader.apiKey,
       watch: config.watch,
-      candles: [ candle ]
+      candles: this.candles
     }
   })
     .then(r => {
       if(r.data.success === false) {
         console.log('error uploading:', r.data);
       }
+      console.log(new Date, 'uploaded', amount, 'candles');
+      this.schedule();
     })
     .catch(e => {
       console.log('error uploading:', e.message);
+      this.schedule();
     });
 
-  done();
-};
+  this.candles = [];
+}
+
+CandleUploader.prototype.finish = function(next) {
+  this.upload();
+  clearTimeout(this.timer);
+}
 
 module.exports = CandleUploader;
